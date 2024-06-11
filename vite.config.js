478af1a3ef99585a72a1114e4e505b530ba3056f
MitulVashista <vashista.1@iitj.ac.in>
2024-06-12 00:08:17 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio"
+  base: "/MitulVashista.github.io"
   plugins: [react()],
 })
